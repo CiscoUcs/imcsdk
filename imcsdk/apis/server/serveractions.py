e9e0cd383823f07d882d1fd6725bd4fb1035e632
Britt Houser <britthouser@gmail.com>
2016-09-15 23:07:52 -0400

    Poll-wait for server power state change in PowerUp/PowerDown apis (#33)
    * Fix flake8 errors in serveractions.py
    
    Three lines in serveractions.py which were longer than 79
    chars and caused flake8 to fail.  Split those lines so
    flake8 passes now.
    
    * Add polling to power up/down functions
    
    This patch adds logic to the power up/down functions in the apis
    folder to poll the server and return only after the desired power
    state has happened been reached.
    
    Signed-off-by: Britt Houser <bhouser@cisco.com>
    
    * Refactor server polling
    
    The server polling loop is identical between functions except
    for the state its watching for.  So refactored the loop into
    its own function that takes in a state.
    
    Signed-off-by: Britt Houser <bhouser@cisco.com>
    
    * Add polling to graceful power down and power cycle
    
    Added the polling loop to both the graceful power
    down and the power cycle functions.
    
    Signed-off-by: Britt Houser <bhouser@cisco.com>
    
    * Address first round of commends in PR
    
    Changed the error raised to be a ImcOperationError.
    
    Signed-off-by: Britt Houser <bhouser@cisco.com>

--- imcsdk/apis/server/serveractions.py
+++ imcsdk/apis/server/serveractions.py
@@ -32,12 +32,52 @@ def get_server_power_state(handle):
     return rack_mo.oper_power
 
 
-def power_up_server(handle):
+def _wait_for_power_state(handle, state, timeout=60, interval=5):
     """
-    This method will power up the rack server
+    This method should be called after a power state change has been triggered.
+    It will poll the server and return when the desired state is achieved.
 
     Args:
         handle(ImcHandle)
+        state(str)
+
+    Returns:
+        bool
+    """
+    import time
+    from imcsdk.imcexception import ImcOperationError
+
+    # Verify desired state is valid
+    if state not in ("on", "off"):
+        raise ValueError("ERROR invalid state: {0}".format(state))
+
+    # Verify interval not set to zero
+    if interval < 1 or type(interval) is not int:
+        raise ValueError("ERROR: interval must be positive integer")
+
+    wait_time = 0
+    while get_server_power_state(handle) != state:
+        # Raise error if we've reached timeout
+        if wait_time > timeout:
+            raise ImcOperationError(
+                'Power State Change',
+                '{0}: ERROR - Power {1} did not complete within ' +
+                '{2} sec'.format(handle.ip, state, timeout)
+            )
+        # Wait interval sec between checks
+        time.sleep(interval)
+        wait_time += interval
+
+
+def power_up_server(handle, timeout=60, interval=5):
+    """
+    This method will send the server the power up signal, and then polls
+    the server every $interval until either $timeout or it comes online.
+
+    Args:
+        handle(ImcHandle)
+        timeout(int)
+        interval(int)
 
     Returns:
         ComputeRackUnit object
@@ -49,18 +89,29 @@ def power_up_server(handle):
     from imcsdk.mometa.compute.ComputeRackUnit import ComputeRackUnit,\
         ComputeRackUnitConsts
 
-    rack_mo = ComputeRackUnit(parent_mo_or_dn="sys", server_id="1")
-    rack_mo.admin_power = ComputeRackUnitConsts.ADMIN_POWER_UP
-    handle.set_mo(rack_mo)
-    return rack_mo
+    # Turn power on only if not already powered up
+    if get_server_power_state(handle) != "on":
+        rack_mo = ComputeRackUnit(parent_mo_or_dn="sys", server_id="1")
+        rack_mo.admin_power = ComputeRackUnitConsts.ADMIN_POWER_UP
+        handle.set_mo(rack_mo)
+
+    # Poll until the server is powered up
+    _wait_for_power_state(handle, "on", timeout=60, interval=5)
+
+    # Return object with current state
+    return handle.query_dn("sys/rack-unit-1")
 
 
-def power_down_server(handle):
+def power_down_server(handle, timeout=60, interval=5):
     """
-    This method will power down the rack server, even if tasks are running on it
+    This method will power down the rack server, even if tasks are still
+    running on it.  Then polls the server every $interval until either $timeout
+    or it comes online.
 
     Args:
         handle(ImcHandle)
+        timeout(int)
+        interval(int)
 
     Returns:
         ComputeRackUnit object
@@ -72,10 +123,16 @@ def power_down_server(handle):
     from imcsdk.mometa.compute.ComputeRackUnit import ComputeRackUnit,\
         ComputeRackUnitConsts
 
-    rack_mo = ComputeRackUnit(parent_mo_or_dn="sys", server_id="1")
-    rack_mo.admin_power = ComputeRackUnitConsts.ADMIN_POWER_DOWN
-    handle.set_mo(rack_mo)
-    return rack_mo
+    # Turn power off only if not already powered down
+    if get_server_power_state(handle) != "off":
+        rack_mo = ComputeRackUnit(parent_mo_or_dn="sys", server_id="1")
+        rack_mo.admin_power = ComputeRackUnitConsts.ADMIN_POWER_DOWN
+        handle.set_mo(rack_mo)
+
+    # Poll until the server is powered up
+    _wait_for_power_state(handle, "off", timeout=60, interval=5)
+
+    return handle.query_dn("sys/rack-unit-1")
 
 
 def power_down_server_gracefully(handle):
@@ -95,10 +152,16 @@ def power_down_server_gracefully(handle):
     from imcsdk.mometa.compute.ComputeRackUnit import ComputeRackUnit,\
         ComputeRackUnitConsts
 
-    rack_mo = ComputeRackUnit(parent_mo_or_dn="sys", server_id="1")
-    rack_mo.admin_power = ComputeRackUnitConsts.ADMIN_POWER_SOFT_SHUT_DOWN
-    handle.set_mo(rack_mo)
-    return rack_mo
+    # Gracefully power off only if not already powered down
+    if get_server_power_state(handle) != "off":
+        rack_mo = ComputeRackUnit(parent_mo_or_dn="sys", server_id="1")
+        rack_mo.admin_power = ComputeRackUnitConsts.ADMIN_POWER_SOFT_SHUT_DOWN
+        handle.set_mo(rack_mo)
+
+    # Poll until the server is powered up
+    _wait_for_power_state(handle, "off", timeout=60, interval=5)
+
+    return handle.query_dn("sys/rack-unit-1")
 
 
 def power_cycle_server(handle):
@@ -121,7 +184,11 @@ def power_cycle_server(handle):
     rack_mo = ComputeRackUnit(parent_mo_or_dn="sys", server_id="1")
     rack_mo.admin_power = ComputeRackUnitConsts.ADMIN_POWER_CYCLE_IMMEDIATE
     handle.set_mo(rack_mo)
-    return rack_mo
+
+    # Poll until the server is powered up
+    _wait_for_power_state(handle, "on", timeout=60, interval=5)
+
+    return handle.query_dn("sys/rack-unit-1")
 
 
 def locator_led_on(handle):
@@ -138,8 +205,8 @@ def locator_led_on(handle):
         locator_led_on(handle)
     """
 
-    from imcsdk.mometa.equipment.EquipmentLocatorLed import EquipmentLocatorLed,\
-        EquipmentLocatorLedConsts
+    from imcsdk.mometa.equipment.EquipmentLocatorLed \
+        import EquipmentLocatorLed, EquipmentLocatorLedConsts
 
     led_mo = EquipmentLocatorLed(parent_mo_or_dn="sys/rack-unit-1")
     led_mo.admin_state = EquipmentLocatorLedConsts.ADMIN_STATE_ON
@@ -160,8 +227,8 @@ def locator_led_off(handle):
         locator_led_off(handle)
     """
 
-    from imcsdk.mometa.equipment.EquipmentLocatorLed import EquipmentLocatorLed,\
-        EquipmentLocatorLedConsts
+    from imcsdk.mometa.equipment.EquipmentLocatorLed \
+        import EquipmentLocatorLed, EquipmentLocatorLedConsts
 
     led_mo = EquipmentLocatorLed(parent_mo_or_dn="sys/rack-unit-1")
     led_mo.admin_state = EquipmentLocatorLedConsts.ADMIN_STATE_OFF
