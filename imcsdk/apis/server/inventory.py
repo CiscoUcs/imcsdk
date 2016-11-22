c2eb99cb4026202a17ddd44242de2c8fb41abbc8
Swapnil Wagh <waghswapnil@gmail.com>
2016-11-22 08:01:30 +0530

    minor fixes in inventory collection
    Signed-off-by: Swapnil Wagh <waghswapnil@gmail.com>

--- imcsdk/apis/server/inventory.py
+++ imcsdk/apis/server/inventory.py
@@ -36,23 +36,30 @@ def _extract_server_id_from_dn(handle, dn):
     return "--"
 
 
-def write_to_txt_file(temp_str):
+def _get_rack_class_id(handle):
+    from imcsdk.imcexception import ImcValidationException
+
+    if handle.platform == IMC_PLATFORM.TYPE_CLASSIC:
+        return("ComputeRackUnit")
+    elif handle.platform == IMC_PLATFORM.TYPE_MODULAR:
+        return("ComputeServerNode")
+    else:
+        raise ImcValidationException("Invalid platform type:%s detected" %
+                                     handle.platform)
+
+
+def _write_to_txt_file(temp_str):
     with open(INVENTORY_TXT_FILE, "a") as file_handle:
         file_handle.write(temp_str)
 
 
-def get_rack_info_csv(handle):
+def _get_rack_info_csv(handle):
     with open(INVENTORY_CSV_FILE, "a") as write_handle:
         fieldnames = ['Rack-Id', 'Vendor', 'Model', 'Serial', 'UUID']
         writer = csv.DictWriter(write_handle, fieldnames=fieldnames)
         writer.writeheader()
 
-        if handle.platform == IMC_PLATFORM.TYPE_CLASSIC:
-            class_id = "ComputeRackUnit"
-        elif handle.platform == IMC_PLATFORM.TYPE_MODULAR:
-            class_id = "ComputeServerNode"
-
-        racks = handle.query_classid(class_id)
+        racks = handle.query_classid(_get_rack_class_id(handle))
         for rack in racks:
             if rack.presence == "missing":
                 continue
@@ -66,28 +73,28 @@ def get_rack_info_csv(handle):
         writer.writerow({})
 
 
-def get_rack_info_txt(handle):
-    write_to_txt_file("============= RACK-INFO =============\n")
-    write_to_txt_file(
+def _get_rack_info_txt(handle):
+    _write_to_txt_file("============= RACK-INFO =============\n")
+    _write_to_txt_file(
         "RACK-ID  RACK-VENDOR              RACK-MODEL     RACK-SERIAL    "
         "RACK-UUID\n")
 
-    racks = handle.query_classid("ComputeRackUnit")
+    racks = handle.query_classid(_get_rack_class_id(handle))
     for rack in racks:
-        write_to_txt_file(
+        _write_to_txt_file(
             "  %s   %20.20s %20.20s      %10.10s     %30.30s\n" %
             (rack.server_id, rack.vendor, rack.model, rack.serial, rack.uuid))
-    write_to_txt_file("\n")
+    _write_to_txt_file("\n")
 
 
-def get_rack_info(handle, file_format):
+def _get_rack_info(handle, file_format):
     if file_format == "txt":
-        get_rack_info_txt(handle)
+        _get_rack_info_txt(handle)
     elif file_format == "csv":
-        get_rack_info_csv(handle)
+        _get_rack_info_csv(handle)
 
 
-def get_cpu_info_csv(handle):
+def _get_cpu_info_csv(handle):
     with open(INVENTORY_CSV_FILE, "a") as write_handle:
         fieldnames = [
             'Server-Id',
@@ -119,29 +126,29 @@ def get_cpu_info_csv(handle):
         writer.writerow({})
 
 
-def get_cpu_info_txt(handle):
-    write_to_txt_file("============= CPU-INFO =============\n")
-    write_to_txt_file(
+def _get_cpu_info_txt(handle):
+    _write_to_txt_file("============= CPU-INFO =============\n")
+    _write_to_txt_file(
         "CPU-ID   CPU-ARCH   CPU-VENDOR                  CPU-MODEL\n")
 
     cpus = handle.query_classid("ProcessorUnit")
     for cpu in cpus:
         if cpu.presence == "missing":
             continue
-        write_to_txt_file(
+        _write_to_txt_file(
             "  %s %10.10s      %20.20s        %30.30s\n" %
             (cpu.id, cpu.arch, cpu.vendor, cpu.model))
-    write_to_txt_file("\n")
+    _write_to_txt_file("\n")
 
 
-def get_cpu_info(handle, file_format):
+def _get_cpu_info(handle, file_format):
     if file_format == "txt":
-        get_cpu_info_txt(handle)
+        _get_cpu_info_txt(handle)
     elif file_format == "csv":
-        get_cpu_info_csv(handle)
+        _get_cpu_info_csv(handle)
 
 
-def get_mem_info_csv(handle):
+def _get_mem_info_csv(handle):
     with open(INVENTORY_CSV_FILE, "a") as write_handle:
         fieldnames = [
             'Server-Id',
@@ -170,30 +177,30 @@ def get_mem_info_csv(handle):
         writer.writerow({})
 
 
-def get_mem_info_txt(handle):
-    write_to_txt_file("============= DIMM-INFO =============\n")
-    write_to_txt_file(
+def _get_mem_info_txt(handle):
+    _write_to_txt_file("============= DIMM-INFO =============\n")
+    _write_to_txt_file(
         "DIMM-ID Capacity Speed Vendor       Model          Serial")
 
     dimms = handle.query_classid("MemoryUnit")
     for dimm in dimms:
         if dimm.presence == "missing":
             continue
-        write_to_txt_file(
+        _write_to_txt_file(
             "  %s      %.8s    %.5s  %.5s   %.20s %.10s" %
             (dimm.id, dimm.capacity, dimm.clock,
              dimm.vendor, dimm.model, dimm.serial)
         )
 
 
-def get_mem_info(handle, file_format):
+def _get_mem_info(handle, file_format):
     if file_format == "txt":
-        get_mem_info_txt(handle)
+        _get_mem_info_txt(handle)
     elif file_format == "csv":
-        get_mem_info_csv(handle)
+        _get_mem_info_csv(handle)
 
 
-def get_psu_info_csv(handle):
+def _get_psu_info_csv(handle):
     with open(INVENTORY_CSV_FILE, "a") as write_handle:
         fieldnames = ['Psu-Id', 'Vendor', 'Model', 'Serial', 'FW-version']
         writer = csv.DictWriter(write_handle, fieldnames=fieldnames)
@@ -213,29 +220,29 @@ def get_psu_info_csv(handle):
         writer.writerow({})
 
 
-def get_psu_info_txt(handle):
-    write_to_txt_file("============= PSU-INFO =============\n")
-    write_to_txt_file(
+def _get_psu_info_txt(handle):
+    _write_to_txt_file("============= PSU-INFO =============\n")
+    _write_to_txt_file(
         "PSU-ID PSU-PRESENCE    PSU-VENDOR           PSU-MODEL\n")
 
     psus = handle.query_classid("EquipmentPsu")
     for psu in psus:
         if psu.presence == "missing":
             continue
-        write_to_txt_file(
+        _write_to_txt_file(
             "  %s    %.10s        %.20s    %.20ss\n" %
             (psu.id, psu.presence, psu.vendor, psu.model))
-    write_to_txt_file("\n")
+    _write_to_txt_file("\n")
 
 
-def get_psu_info(handle, file_format):
+def _get_psu_info(handle, file_format):
     if file_format == "txt":
-        get_psu_info_txt(handle)
+        _get_psu_info_txt(handle)
     elif file_format == "csv":
-        get_psu_info_csv(handle)
+        _get_psu_info_csv(handle)
 
 
-def get_pci_info_csv(handle):
+def _get_pci_info_csv(handle):
     with open(INVENTORY_CSV_FILE, "a") as write_handle:
         fieldnames = ['Server-Id', 'Pci-Id', 'Vendor', 'Model', 'FW-version']
         writer = csv.DictWriter(write_handle, fieldnames=fieldnames)
@@ -253,29 +260,29 @@ def get_pci_info_csv(handle):
         writer.writerow({})
 
 
-def get_pci_info_txt(handle):
+def _get_pci_info_txt(handle):
 
-    write_to_txt_file("============= PCI-INFO =============\n")
-    write_to_txt_file(
+    _write_to_txt_file("============= PCI-INFO =============\n")
+    _write_to_txt_file(
         "PCI-ID VENDOR       MODEL                                   "
         "FW-VERSION\n")
 
     pci_devices = handle.query_classid("PciEquipSlot")
     for device in pci_devices:
-        write_to_txt_file(
+        _write_to_txt_file(
             "%.5s      %.20s   %.40s     %.10s\n" %
             (device.id, device.vendor, device.model, device.version))
-    write_to_txt_file("\n")
+    _write_to_txt_file("\n")
 
 
-def get_pci_info(handle, file_format):
+def _get_pci_info(handle, file_format):
     if file_format == "txt":
-        get_pci_info_txt(handle)
+        _get_pci_info_txt(handle)
     elif file_format == "csv":
-        get_pci_info_csv(handle)
+        _get_pci_info_csv(handle)
 
 
-def get_vic_info_csv(handle):
+def _get_vic_info_csv(handle):
     with open(INVENTORY_CSV_FILE, "a") as write_handle:
         fieldnames = ['Server-Id', 'Adaptor-Id', 'Vendor',
                       'Model', 'Serial', 'PCI-Slot']
@@ -295,27 +302,27 @@ def get_vic_info_csv(handle):
         writer.writerow({})
 
 
-def get_vic_info_txt(handle):
-    write_to_txt_file("============= VIC-INFO =============\n")
-    write_to_txt_file(
+def _get_vic_info_txt(handle):
+    _write_to_txt_file("============= VIC-INFO =============\n")
+    _write_to_txt_file(
         "ADAPTOR-ID  VENDOR              MODEL               PCI-SLOT\n")
 
     adaptors = handle.query_classid("AdaptorUnit")
     for adaptor in adaptors:
-        write_to_txt_file(
+        _write_to_txt_file(
             "  %s         %.20s   %.20s     %.5s\n" %
             (adaptor.id, adaptor.vendor, adaptor.model, adaptor.pci_slot))
-    write_to_txt_file("\n")
+    _write_to_txt_file("\n")
 
 
-def get_vic_info(handle, file_format):
+def _get_vic_info(handle, file_format):
     if file_format == "txt":
-        get_vic_info_txt(handle)
+        _get_vic_info_txt(handle)
     elif file_format == "csv":
-        get_vic_info_csv(handle)
+        _get_vic_info_csv(handle)
 
 
-def get_lom_info_csv(handle):
+def _get_lom_info_csv(handle):
     with open(INVENTORY_CSV_FILE, "a") as write_handle:
         fieldnames = ['Server-Id', 'LOM-Adaptor-Id',
                       'Model', 'PCI-Slot', 'No. Interfaces']
@@ -336,29 +343,29 @@ def get_lom_info_csv(handle):
         writer.writerow({})
 
 
-def get_lom_info_txt(handle):
-    write_to_txt_file("============= NETWORK-ADAPTOR-INFO =============\n")
-    write_to_txt_file(
+def _get_lom_info_txt(handle):
+    _write_to_txt_file("============= NETWORK-ADAPTOR-INFO =============\n")
+    _write_to_txt_file(
         "LOM-ADAPTOR-ID       MODEL             PCI-SLOT   #INTERFACES\n")
 
     adaptors = handle.query_classid("NetworkAdapterUnit")
     ctr = 1
     for adaptor in adaptors:
-        write_to_txt_file(
+        _write_to_txt_file(
             "  %s          %.20s      %.5s         %.5s\n" %
             (str(ctr), adaptor.model, adaptor.slot, adaptor.num_intf))
         ctr += 1
-    write_to_txt_file("\n")
+    _write_to_txt_file("\n")
 
 
-def get_lom_info(handle, file_format):
+def _get_lom_info(handle, file_format):
     if file_format == "txt":
-        get_lom_info_txt(handle)
+        _get_lom_info_txt(handle)
     elif file_format == "csv":
-        get_lom_info_csv(handle)
+        _get_lom_info_csv(handle)
 
 
-def get_storage_info_csv(handle):
+def _get_storage_info_csv(handle):
     with open(INVENTORY_CSV_FILE, "a") as write_handle:
         fieldnames = [
             'Server-Id',
@@ -405,9 +412,9 @@ def get_storage_info_csv(handle):
         writer.writerow({})
 
 
-def get_storage_info_txt(handle):
-    write_to_txt_file("============= STORAGE-CONTROLLER-INFO =============\n")
-    write_to_txt_file(
+def _get_storage_info_txt(handle):
+    _write_to_txt_file("============= STORAGE-CONTROLLER-INFO =============\n")
+    _write_to_txt_file(
         "ID      TYPE            VENDOR        MODEL               "
         "SERIAL     PCI-SLOT   FIRMWARE\n")
 
@@ -417,7 +424,7 @@ def get_storage_info_txt(handle):
         prop = handle.query_children(
             in_dn=controller.dn,
             class_id="StorageControllerProps")
-        write_to_txt_file(
+        _write_to_txt_file(
             " %s      %.10s        %.10s   "
             "%.30s     %.10s   %.5s     %.10s\n" %
             (str(ctr),
@@ -431,7 +438,7 @@ def get_storage_info_txt(handle):
 
     flex_controllers = handle.query_classid("StorageFlexFlashController")
     for controller in flex_controllers:
-        write_to_txt_file(
+        _write_to_txt_file(
             " %s      %.10s     %.10s   %.30s"
             "     %.10s   %.5s        %.10s\n" %
             (str(ctr),
@@ -442,17 +449,17 @@ def get_storage_info_txt(handle):
              "N/A",
              controller.fw_version))
         ctr += 1
-    write_to_txt_file("\n")
+    _write_to_txt_file("\n")
 
 
-def get_storage_info(handle, file_format):
+def _get_storage_info(handle, file_format):
     if file_format == "txt":
-        get_storage_info_txt(handle)
+        _get_storage_info_txt(handle)
     elif file_format == "csv":
-        get_storage_info_csv(handle)
+        _get_storage_info_csv(handle)
 
 
-def get_tpm_info_csv(handle):
+def _get_tpm_info_csv(handle):
     with open(INVENTORY_CSV_FILE, "a") as write_handle:
         fieldnames = ['Server-Id', 'Tpm-Id', 'Vendor',
                       'Model', 'Serial', 'Revision']
@@ -476,39 +483,39 @@ def get_tpm_info_csv(handle):
         writer.writerow({})
 
 
-def get_tpm_info_txt(handle):
-    write_to_txt_file("============= NETWORK-ADAPTOR-INFO =============\n")
-    write_to_txt_file("TPM-ID \tVENDOR \tMODEL \tSERIAL \tREVISION\n")
+def _get_tpm_info_txt(handle):
+    _write_to_txt_file("============= NETWORK-ADAPTOR-INFO =============\n")
+    _write_to_txt_file("TPM-ID \tVENDOR \tMODEL \tSERIAL \tREVISION\n")
 
     tpms = handle.query_classid("EquipmentTpm")
     ctr = 1
     for tpm in tpms:
         if tpm.presence in ['missing', 'empty']:
             continue
-        write_to_txt_file(
+        _write_to_txt_file(
             "  %s \t%s \t%s \t%s \t%s\n" %
             (str(ctr), tpm.vendor, tpm.model, tpm.serial, tpm.tpm_revision))
         ctr += 1
-    write_to_txt_file("\n")
+    _write_to_txt_file("\n")
 
 
-def get_tpm_info(handle, file_format):
+def _get_tpm_info(handle, file_format):
     if file_format == "txt":
-        get_tpm_info_txt(handle)
+        _get_tpm_info_txt(handle)
     elif file_format == "csv":
-        get_tpm_info_csv(handle)
+        _get_tpm_info_csv(handle)
 
 
-def delete_inventory_files(file_format):
+def _delete_inventory_files(file_format):
     import os
 
-    if file_format == "txt":
-        cmd = "rm -f " + INVENTORY_TXT_FILE
-    elif file_format == "csv":
-        cmd = "rm -f " + INVENTORY_CSV_FILE
+    if file_format == "txt" or file_format == "csv":
+        file_name = "INVENTORY_" + file_format.upper() + "_FILE"
     else:
         return
-    os.system(cmd)
+
+    if os.path.exists(file_name):
+        os.remove(file_name)
 
 
 def get_inventory(handle, types="all", file_format="csv"):
@@ -527,18 +534,18 @@ def get_inventory(handle, types="all", file_format="csv"):
     """
 
     callback_dict = {
-        'rack': get_rack_info,
-        'cpu': get_cpu_info,
-        'memory': get_mem_info,
-        'psu': get_psu_info,
-        'pci': get_pci_info,
-        'vic': get_vic_info,
-        'lom': get_lom_info,
-        'storage': get_storage_info,
-        'tpm': get_tpm_info
+        'rack': _get_rack_info,
+        'cpu': _get_cpu_info,
+        'memory': _get_mem_info,
+        'psu': _get_psu_info,
+        'pci': _get_pci_info,
+        'vic': _get_vic_info,
+        'lom': _get_lom_info,
+        'storage': _get_storage_info,
+        'tpm': _get_tpm_info
     }
 
-    delete_inventory_files(file_format)
+    _delete_inventory_files(file_format)
 
     if types == "all":
         item_list = [
