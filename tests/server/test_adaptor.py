62931368aec485bf5d9ba8f89ec46c61158fa387
Swapnil Wagh <waghswapnil@gmail.com>
2017-02-06 11:23:23 +0530

    refactor adaptor apis and bug fix for firmware upgrade monitoring
    Signed-off-by: Swapnil Wagh <waghswapnil@gmail.com>

--- tests/server/test_adaptor.py
+++ tests/server/test_adaptor.py
@@ -11,16 +11,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import time
 from nose.tools import assert_equal, assert_not_equal
 from ..connection.info import custom_setup, custom_teardown
-from imcsdk.apis.server.adaptor import setup_vic_adaptor_properties, \
-    get_vic_adaptor_properties, create_vnic, delete_vnic, get_vnic, \
-    create_vhba, delete_vhba, get_vhba
+from imcsdk.apis.server.adaptor import adaptor_properties_set, \
+    adaptor_properties_get, adaptor_reset, vnic_create, vnic_delete, vnic_get, \
+    vhba_create, vhba_delete, vhba_get
 
 
 handle = None
 ADAPTOR_ID = "1"
-# ADAPTOR_ID = "MLOM"
+ADAPTOR_ID = "MLOM"
+# ADAPTOR_ID = "SIOC1"
 
 
 def setup_module():
@@ -33,78 +35,75 @@ def teardown_module():
 
 
 def test_non_vntag_mode():
-    global handle
-    setup_vic_adaptor_properties(handle, adaptor_slot=ADAPTOR_ID, fip_mode=True,
+    adaptor_properties_set(handle, adaptor_slot=ADAPTOR_ID, fip_mode=True,
                                  vntag_mode=False)
-    mo = get_vic_adaptor_properties(handle, adaptor_slot=ADAPTOR_ID)
+    mo = adaptor_properties_get(handle, adaptor_slot=ADAPTOR_ID)
     assert_equal(mo.vntag_mode.lower(), "disabled")
 
 
 def test_non_vntag_create_vnic():
-    global handle
-    vnic_mo = create_vnic(handle, adaptor_slot=ADAPTOR_ID, name="sdk-test-vnic-nv",
+    vnic_mo = vnic_create(handle, adaptor_slot=ADAPTOR_ID, name="sdk-test-vnic-nv",
                           class_of_service=5, mac="00:11:22:33:44:56",
                           mtu=1500, pxe_boot=True, uplink_port=0)
-    rcvd_mo = get_vnic(handle, adaptor_slot=ADAPTOR_ID, name="sdk-test-vnic-nv")
+    rcvd_mo = vnic_get(handle, adaptor_slot=ADAPTOR_ID, name="sdk-test-vnic-nv")
     assert_not_equal(rcvd_mo, None)
     assert_equal(vnic_mo.name, rcvd_mo.name)
     assert_equal(vnic_mo.class_of_service, rcvd_mo.class_of_service)
 
 
-def test_delete_vnic():
-    global handle
-    delete_vnic(handle, adaptor_slot=ADAPTOR_ID, name="sdk-test-vnic-nv")
-    assert_equal(get_vnic(handle, adaptor_slot=ADAPTOR_ID, name="sdk-test-vnic-nv"), None)
+def test_non_vntag_delete_vnic():
+    vnic_delete(handle, adaptor_slot=ADAPTOR_ID, name="sdk-test-vnic-nv")
+    assert_equal(vnic_get(handle, adaptor_slot=ADAPTOR_ID, name="sdk-test-vnic-nv"), None)
 
 
-def test_enable_adaptor_properties():
-    global handle
-    setup_vic_adaptor_properties(handle, adaptor_slot=ADAPTOR_ID, fip_mode=True,
-                                 vntag_mode=True, num_vmfex_ifs=5)
-    mo = get_vic_adaptor_properties(handle, adaptor_slot=ADAPTOR_ID)
+def test_vntag_mode():
+    adaptor_properties_set(handle, adaptor_slot=ADAPTOR_ID, fip_mode=True,
+                                 vntag_mode=True, num_vmfex_ifs=5, lldp=True)
+    adaptor_reset(handle, adaptor_slot=ADAPTOR_ID)
+    time.sleep(5)
+    mo = adaptor_properties_get(handle, adaptor_slot=ADAPTOR_ID)
     assert_equal(mo.vntag_mode.lower(), "enabled")
     assert_equal(mo.fip_mode.lower(), "enabled")
+    assert_equal(mo.lldp.lower(), "enabled")
 
 
-def test_create_vnic():
-    global handle
-    vnic_mo = create_vnic(handle, adaptor_slot=ADAPTOR_ID, name="sdk-test-vnic",
+def test_vntag_create_vnic():
+    vnic_mo = vnic_create(handle, adaptor_slot=ADAPTOR_ID, name="sdk-test-vnic",
                           channel_number=100, mac="00:11:22:33:44:55",
                           mtu=1500, pxe_boot=True, uplink_port=0)
-    rcvd_mo = get_vnic(handle, adaptor_slot=ADAPTOR_ID, name="sdk-test-vnic")
+    rcvd_mo = vnic_get(handle, adaptor_slot=ADAPTOR_ID, name="sdk-test-vnic")
     assert_not_equal(rcvd_mo, None)
     assert_equal(vnic_mo.name, rcvd_mo.name)
     assert_equal(vnic_mo.channel_number, rcvd_mo.channel_number)
 
 
-def test_delete_vnic():
-    global handle
-    delete_vnic(handle, adaptor_slot=ADAPTOR_ID, name="sdk-test-vnic")
-    assert_equal(get_vnic(handle, adaptor_slot=ADAPTOR_ID, name="sdk-test-vnic"), None)
+def test_vntag_delete_vnic():
+    vnic_delete(handle, adaptor_slot=ADAPTOR_ID, name="sdk-test-vnic")
+    assert_equal(vnic_get(handle, adaptor_slot=ADAPTOR_ID, name="sdk-test-vnic"), None)
 
 
-def test_create_vhba():
-    global handle
-    vhba_mo = create_vhba(handle, adaptor_slot=ADAPTOR_ID, name="sdk-test-vhba",
+def test_vntag_create_vhba():
+    vhba_mo = vhba_create(handle, adaptor_slot=ADAPTOR_ID, name="sdk-test-vhba",
                           channel_number=101, wwnn="10:00:11:3A:7D:D0:9A:43",
                           wwpn="20:00:11:3A:7D:D0:9A:43",
                           san_boot=True, uplink_port=0)
-    rcvd_mo = get_vhba(handle, adaptor_slot=ADAPTOR_ID, name="sdk-test-vhba")
+    rcvd_mo = vhba_get(handle, adaptor_slot=ADAPTOR_ID, name="sdk-test-vhba")
     assert_not_equal(rcvd_mo, None)
     assert_equal(vhba_mo.name, rcvd_mo.name)
     assert_equal(vhba_mo.channel_number, rcvd_mo.channel_number)
 
 
-def test_delete_vhba():
-    global handle
-    delete_vhba(handle, adaptor_slot=ADAPTOR_ID, name="sdk-test-vhba")
-    assert_equal(get_vhba(handle, adaptor_slot=ADAPTOR_ID, name="sdk-test-vhba"), None)
+def test_vntag_delete_vhba():
+    vhba_delete(handle, adaptor_slot=ADAPTOR_ID, name="sdk-test-vhba")
+    assert_equal(vhba_get(handle, adaptor_slot=ADAPTOR_ID, name="sdk-test-vhba"), None)
 
 
-def test_disable_adaptor_properties():
-    global handle
-    setup_vic_adaptor_properties(handle, adaptor_slot=ADAPTOR_ID, fip_mode=False,
-                                 vntag_mode=False, num_vmfex_ifs=5)
-    mo = get_vic_adaptor_properties(handle, adaptor_slot=ADAPTOR_ID)
+def test_disable_vntag_mode():
+    adaptor_properties_set(handle, adaptor_slot=ADAPTOR_ID, fip_mode=False,
+                                 vntag_mode=False, num_vmfex_ifs=5, lldp=False)
+    adaptor_reset(handle, adaptor_slot=ADAPTOR_ID)
+    time.sleep(5)
+    mo = adaptor_properties_get(handle, adaptor_slot=ADAPTOR_ID)
     assert_equal(mo.vntag_mode.lower(), "disabled")
     assert_equal(mo.fip_mode.lower(), "disabled")
+    assert_equal(mo.lldp.lower(), "disabled")
